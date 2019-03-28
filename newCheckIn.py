3734b089a2457707d233217532af2dddd8d1dfe0
yunqiang <yunqiang@Yunqiang.local>
2019-03-28 11:31:24 +0800

    增加异常处理 跳过密码错误的账号

--- newCheckIn.py
+++ newCheckIn.py
@@ -25,22 +25,25 @@ login_headers = {
 }
 page_json = json.loads(r.text)
 for i in range(page_json['length']):
-    s=page_json['data'][i]['studentNum']
-    getTokenURL='https://seat.ujn.edu.cn:8443/rest/auth?username='+page_json['data'][i]['studentNum']+'&password='+page_json['data'][i]['password']
-    tokenResponse=requests.get(getTokenURL,headers=login_headers,verify=False)
-    login_json = json.loads(tokenResponse.text)
-    token=login_json['data']['token']
-    checkInURL='https://seat.ujn.edu.cn:8443/rest/v2/checkIn'
-    headers = {
-        'Host': 'seat.ujn.edu.cn:8443',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'doSingle/11 CFNetwork/811.5.4 Darwin/16.6.0',
-        'Accept-Language': 'zh-cn',
-        'token': token,
-        'Accept-Encoding': 'gzip',
-        'X-Forwarded-For': '10.167.159.118'
-    }
-    page=requests.get(checkInURL,headers=headers,verify=False)
-    print(page.text)
+    try:
+        s=page_json['data'][i]['studentNum']
+        getTokenURL='https://seat.ujn.edu.cn:8443/rest/auth?username='+page_json['data'][i]['studentNum']+'&password='+page_json['data'][i]['password']
+        tokenResponse=requests.get(getTokenURL,headers=login_headers,verify=False)
+        login_json = json.loads(tokenResponse.text)
+        token=login_json['data']['token']
+        checkInURL='https://seat.ujn.edu.cn:8443/rest/v2/checkIn'
+        headers = {
+            'Host': 'seat.ujn.edu.cn:8443',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Connection': 'keep-alive',
+            'Accept': '*/*',
+            'User-Agent': 'doSingle/11 CFNetwork/811.5.4 Darwin/16.6.0',
+            'Accept-Language': 'zh-cn',
+            'token': token,
+            'Accept-Encoding': 'gzip',
+            'X-Forwarded-For': '10.167.159.118'
+        }
+        page=requests.get(checkInURL,headers=headers,verify=False)
+        print(page.text)
+    except:
+        continue
--- newSeatCrawler.py
+++ newSeatCrawler.py
@@ -26,33 +26,35 @@ login_headers = {
 }
 
 for i in range(page_json['length']):
-    print (page_json['data'][i]['studentNum'])
-    getTokenURL='https://seat.ujn.edu.cn:8443/rest/auth?username='+page_json['data'][i]['studentNum']+'&password='+page_json['data'][i]['password']
-    tokenResponse=requests.get(getTokenURL,headers=login_headers,verify=False)
-    login_json = json.loads(tokenResponse.text)
-    token=login_json['data']['token']
+    try:
+        getTokenURL='https://seat.ujn.edu.cn:8443/rest/auth?username='+page_json['data'][i]['studentNum']+'&password='+page_json['data'][i]['password']
+        tokenResponse=requests.get(getTokenURL,headers=login_headers,verify=False)
+        login_json = json.loads(tokenResponse.text)
+        token=login_json['data']['token']
 
-    date = datetime.date.today()
-    date += datetime.timedelta(days=1)
-    strdate = date.strftime('%Y-%m-%d')
-    headers = {
-        'Host': 'seat.ujn.edu.cn:8443',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'token': login_json['data']['token'],
-        'User-Agent': 'doSingle/11 CFNetwork/811.5.4 Darwin/16.6.0',
-        'Accept-Language': 'zh-cn',
-        'X-Forwarded-For': '10.167.159.118'
-    }
-    postdata={
-        't':1,
-        'startTime':page_json['data'][i]['startTime'],
-        'endTime':page_json['data'][i]['endTime'],
-        'date':strdate,
-        'seat':page_json['data'][i]['seatId'],
-        't2':2
-    }
-    mainURL='https://seat.ujn.edu.cn:8443/rest/v2/freeBook'
-    s=requests.post(mainURL,data=postdata,headers=headers,verify=False)
-    print(s.text)
+        date = datetime.date.today()
+        date += datetime.timedelta(days=1)
+        strdate = date.strftime('%Y-%m-%d')
+        headers = {
+            'Host': 'seat.ujn.edu.cn:8443',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Connection': 'keep-alive',
+            'Accept': '*/*',
+            'token': login_json['data']['token'],
+            'User-Agent': 'doSingle/11 CFNetwork/811.5.4 Darwin/16.6.0',
+            'Accept-Language': 'zh-cn',
+            'X-Forwarded-For': '10.167.159.118'
+        }
+        postdata={
+            't':1,
+            'startTime':page_json['data'][i]['startTime'],
+            'endTime':page_json['data'][i]['endTime'],
+            'date':strdate,
+            'seat':page_json['data'][i]['seatId'],
+            't2':2
+        }
+        mainURL='https://seat.ujn.edu.cn:8443/rest/v2/freeBook'
+        s=requests.post(mainURL,data=postdata,headers=headers,verify=False)
+        print(s.text)
+    except:
+        continue
